8950579af6b0d5771f6f3bb9e56ff016839ff766
Connor Leech <connor@stitchlabs.com>
2018-10-16 11:32:19 -0700

    add array chunking

--- udemy-interview-bootcamp-course/array_chunking.js
+++ udemy-interview-bootcamp-course/array_chunking.js
@@ -9,8 +9,69 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
+	let currentChunk = 0;
+	let result       = [];
+	let chunk        = [];
 
+	for(var i = 0; i < array.length; i++){
+		if(currentChunk == size){
+			result.push(chunk);
+			currentChunk = 0;
+			chunk        = [];
+		}
+
+		currentChunk++;
+		chunk.push(array[i]);
+	}
+
+	if(chunk.length) result.push(chunk);
+
+	return result;
 }
 
 
-console.log(chunk([1, 2, 3, 4], 2));
+function chunkSolution1(array, size){
+	// create empty array to hold all other arrays
+	const chunked = [];
+
+	for(let element of array){
+		// retrieve the last element in the chunked array
+		const last = chunked[chunked.length - 1];
+
+		// if there is no last element or it's length is the size
+		if(!last || last.length === size){
+
+			// add a new chunk with the current element
+			chunked.push([element]);
+		} else {
+			// add the current element into the chunk
+			last.push(element);
+		}
+	}
+
+	return chunked;
+}
+
+
+// Rolling window with startIndex variable
+// Good practice for using Array.prototype.slice
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/slice
+function chunkSolution2(array, size){
+	let startIndex = 0;
+	let output     = [];
+
+	while(startIndex < array.length){
+
+		output.push(array.slice(startIndex, startIndex + size));
+
+		startIndex += size;
+	}
+
+	return output;	
+}
+
+
+
+console.log(chunkSolution2([1, 2, 3, 4], 2));
+console.log(chunkSolution2([1, 2, 3, 4, 5, 6, 7, 8], 3));
+console.log(chunkSolution2([1, 2, 3, 4, 5], 4));
