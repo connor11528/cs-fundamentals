723a5b2f48e4d77244d6a949ba9f55e7f7694ea3
Connor Leech <connor@stitchlabs.com>
2018-11-05 16:13:13 -0800

    add pyramids recursive

--- udemy-interview-bootcamp-course/pyramids.js
+++ udemy-interview-bootcamp-course/pyramids.js
@@ -15,13 +15,13 @@
 //       '#####'
 
 function pyramids(height){
-	const midpoint = Math.floor((height + height - 1) / 2);
+	const midpoint = Math.floor((2 * height - 1) / 2);
 	
 
 	for(var row = 0; row < height; row++){
 		let block = '';
 
-		for(var column = 0; column < height + height - 1; column++){
+		for(var column = 0; column < 2 * height - 1; column++){
 
 			block += (midpoint - row <= column && midpoint + row >= column) ? '#' : ' ';
 		}
@@ -31,9 +31,25 @@ function pyramids(height){
 }
 
 
-function pyramidsRecursive(height){
-	
+function pyramidsRecursive(height, row = 0, level = ''){
+	if(row === height) return;
+
+	if(level.length === 2 * height - 1){
+		console.log(level);
+		return pyramidsRecursive(height, row + 1);
+	}
+
+	const midpoint = Math.floor((2 * height - 1) / 2);
+	let add;
+
+	if(midpoint - row <= level.length && midpoint + row >= level.length){
+		add = '#';
+	} else {
+		add = ' ';
+	}
+
+	pyramidsRecursive(height, row, level + add);
 
 }
 
-pyramids(3);
+pyramidsRecursive(3);
