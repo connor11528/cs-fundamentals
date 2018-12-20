c98dc258d0783f61baa223e3e3b8f939bfc89db2
Connor Leech <connor@stitchlabs.com>
2018-12-20 15:37:05 -0800

    [ trees ] add depth first search

--- udemy-interview-bootcamp-course/data_structures/trees.js
+++ udemy-interview-bootcamp-course/data_structures/trees.js
@@ -20,7 +20,51 @@ class Tree {
   constructor(){
     this.root = null;
   }
+
+  breadthFirstTraverse(){
+    const queue = [this.root]; // first in, first out
+
+    while(queue.length){
+      // remove first element from queue
+      const first = queue.shift();
+
+      // log out the element
+      console.log(first.data);
+
+      // ES6 Spread syntax example:
+      //===========================
+      // var parts = ['shoulders', 'knees']; 
+      // var lyrics = ['head', ...parts, 'and', 'toes']; 
+      // outputs: ["head", "shoulders", "knees", "and", "toes"]
+
+      // add each element of children into the queue
+      queue.push(...first.children);
+
+    }
+
+  }
+
+  depthFirstTraverse(){
+    const stack = [this.root];
+
+    while(stack.length){
+      const first = stack.shift();
+
+      console.log(first.data);
+
+      stack.unshift(...first.children);
+    }
+    
+  }
 }
 
+const t = new Tree();
+t.root= new Node('a');
+t.root.add('b');
+t.root.add('d');
+t.root.children[0].add('c');
+
+
+t.depthFirstTraverse();
 
 module.exports = { Tree, Node };
